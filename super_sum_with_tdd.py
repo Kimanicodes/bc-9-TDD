3e8033c480b96fd78685166b556b5ed418f44a4d
mrkimanindegwa <mrkimanindegwa@gmail.com>
2016-08-18 18:40:41 +0300

    Adds folder for super sum with the Implementation and tests

---
+++ super_sum_with_tdd.py
@@ -0,0 +1,20 @@
+def super_sum(*args):  # using args for the function
+    """Super Sum: A function that"""
+
+    total_list = 0
+    total_int = 0
+    for argument in args:
+        if type(argument) is list:
+            for number in argument:
+                if type(number) is int:
+                        total_list = total_list + number
+                else:
+                    return "Your list has non integer items"
+        else:
+            if type(argument) is int:
+                total_int = total_int + argument
+            else:
+                return None
+    total = total_int + total_list
+    return total
+
