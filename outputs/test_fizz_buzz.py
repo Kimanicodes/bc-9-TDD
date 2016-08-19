fcf596416bbe36f73702c187a4c6440cc5113d75
mrkimanindegwa <mrkimanindegwa@gmail.com>
2016-08-19 08:49:56 +0300

    Adds FizzBuzz Tests to 10

--- outputs/test_fizz_buzz.py
+++ outputs/test_fizz_buzz.py
@@ -16,6 +16,28 @@ class TestFizzBuzz(unittest.TestCase):
     def test_returns_number_if_above_fail(self):
         self.assertEqual(fizz_buzz(29), 29)
 
+    def test_format_exactly_equal(self):
+        self.assertNotEqual(fizz_buzz(6), 'FIZZ')
+        self.assertNotEqual(fizz_buzz(25), 'buzz')
+        self.assertNotEqual(fizz_buzz(30), 'fizzBuzz')
+
+    def test_result_is_string(self):
+        result1 = fizz_buzz(12)
+        self.assertIs(result1, str)
+
+    def test_result_for_number_is_int(self):
+        self.assertNotEqual(fizz_buzz(17, '17'))
+
+    def test_error_raised(self):
+        with self.assertRaises(TypeError):
+            fizz_buzz('')
+
+    def test_result_is_returned(self):
+        self.assertTrue(fizz_buzz(12))
+
+    def test_returned_value_is_an_integer(self):
+        self.assertEqual(type(fizz_buzz(15)), int)
+
 
 if __name__ == "__main__":
     unittest.main()
